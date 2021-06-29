2f36f5236a32ca10ec9d5d37d6fcf49bfc03c75b
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2021-06-29 16:14:45 +0300

    new project

---
+++ EVA Laser Studio/js/main.js
@@ -0,0 +1,70 @@
+
+
+
+
+$('.certificates-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow:"<button class='slick-next'><img src='img/slider-arrow-right.png'></button>",
+  prevArrow:"<button class='slick-prev'><img src='img/slider-arrow-left.png'></button>",
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+  $('.slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow:"<button class='slick-next'><img src='img/slider-arrow-right.png'></button>",
+  prevArrow:"<button class='slick-prev'><img src='img/slider-arrow-left.png'></button>",
+  
+});
+} 
